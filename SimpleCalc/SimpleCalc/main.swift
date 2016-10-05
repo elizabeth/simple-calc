13a02a79754a71098bab468cd3209c1d64d835e3
luludust <elizabethduong96@gmail.com>
2016-10-04 18:47:52 -0700

    Initial commit

---
+++ SimpleCalc/SimpleCalc/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by iGuest on 10/4/16.
+//  Copyright © 2016 Elizabeth. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+func count(numbers: String) -> Int {
+    return 4
+}
+
+//let response = readLine(stripNewLine: true)!
+//
+//let num = UInt.init(response)!
