041f08178306dfb1ed27e56bf0c5dbe766d449a2
luludust <elizabethduong96@gmail.com>
2016-10-06 02:10:33 -0700

    Some code changes

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -22,13 +22,13 @@ func calculate(first: Double, second: Double, op: String) {
         case "%":
             print("Result: \(first.truncatingRemainder(dividingBy: second))")
         default:
-            print("Must enter a valid operation")
+            print("Must enter a valid operation: +, -, *, /, %")
     }
 }
 
 func multiOperation(array: Array<String>) {
     let operand = array[array.count - 1]
-    let numLength = array.count - 2;
+    let numLength = array.count - 1;
     
     switch operand {
         case "count":
@@ -36,72 +36,81 @@ func multiOperation(array: Array<String>) {
         case "avg":
             var avg = 0
             
-            for i in 0...numLength {
+            for i in 0...<numLength {
                 avg += Int(array[i])!
             }
     
-            print("Result: \(avg)")
+            print("Result: \(avg/(numLength))")
         case "fact":
-            let first = Int(array[0])
-            if (numLength == 1) {
-                print("Factorial can only accept one number")
-            } else if first! < 0 {
+            let first = Int(array[0])!
+            if first == nil{
+            	print("Factorial number must be an integer") 
+            } else if numLength > 1 {    
+                print("Factorial can only accept one number"  
+            } else if first < 0 {
                 print("Factorial number must be 0 or greater")
+            } else if first == 0 {
+            	print("Result: 1")
             } else {
-                guard let first = Int(array[0]) else {
-                    print("Factorial number must be an integer")
-                    exit(0)
-                }
-            
                 var result = first
-                var temp = first
-                repeat {
+                var temp = first - 1
+                while temp > 0 {
+                    result = result * temp
+
                     temp = temp - 1
-                    result = result * first
-                } while (temp > 0)
+                }
+
+                print("Result: \(result)")
             }
+        default:
+        	print("Must enter a valid operation: count, avg, fact")
     }
 }
 
-print("Enter an expression separated by returns or a multi-operand operation:")
+func getValidNum() -> Double {
+	var num: T? = nil
+	do {
+		print("Must enter a valid number")
+		num = readLine(strippingNewLine: true)!
+	} while (num == nil)
 
-if let response = readLine(stripNewLine: true)! {
-	var input = split(response!) {$0 == " "}
-
-	// check if multiple arguments entered in first input (normal operation) or multioperand operation
-	if input.count == 1 {
-		
-		var first = Double(input[0]) 
-		while first == nil {
-			print("Must enter a valid number")
-			let num = readLine(stripNewLine: true)!
-			first = Double(input[0])
-		}
-
-		if let operation = readLine(stripNewLine: true) {
-			if let inputSecond = readLine(stringNewLine: true) {
-				guard let second = Double(inputSecond) else {
-					print("Must enter a valid number")
-				}
-				
-				calculate(first, second, operation)
-			} else {
-				print("Please enter a number")
-			}
-		} else {
-			print("Please enter an operation")
-			// 
-		}
-	} else {
-		multiOperation(input)
-	}
-} else {
-    print("Nothing")
+	return Double(num)
 }
 
-exit(0)
+// This runs first
+while true {
+	print("Enter an expression separated by returns or a multi-operand operation/nType exit to exit:")
 
+	if let response = readLine(strippingNewLine: true) {
+		var input = response.components(separatedBy: " ")
 
+		// check if multiple arguments entered in first input (normal operation) or multioperand operation
+		if input.count == 1 {
+			if (input[0] == "exit") {
+				exit(0)
+			}
 
-//
-//let num = UInt.init(response)!
+			var first = Double(input[0]) 
+			if first == nil {
+				first = getValidNum
+			}
+
+			if let operation = readLine(strippingNewline: true) {
+				if let inputSecond = readLine(strippingNewLine: true) {
+					guard var second = Double(inputSecond) if second == nil {
+						second = getValidNum
+					}
+					
+					calculate(first, second, operation)
+				} else {
+					print("Please enter a number")
+				}
+			} else {
+				print("Please enter an operation")
+				// 
+			}
+		} else {
+			multiOperation(input)
+		}
+	}
+}
